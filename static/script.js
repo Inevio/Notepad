01410a85ed9001573a366d63b0ee7e258328752c
Carlos Navas <carlosnavas1991@gmail.com>
2018-03-12 15:55:02 +0100

    WIP

---
+++ static/script.js
@@ -0,0 +1,40 @@
+// console.log(params)
+
+var openFile = function (fileId) {
+  api.fs(fileId, (error, file) => {
+    if (error) return console.error(error)
+    file.read((error, result) => {
+      if (error) return console.error(error)
+      $('.file-name').text(file.name)
+      $('.text').val(result.toString())
+    })
+  })
+}
+
+$('.text').on('keydown', function (event) {
+  if (event.keyCode === 9) {
+    // Set up some variables. We need to know the current position of the cursor or selection.
+    var selectionStartPos = this.selectionStart
+    var selectionEndPos = this.selectionEnd
+    var oldContent = this.value
+
+    // Set the new content.
+    this.value = oldContent.substring(0, selectionStartPos) + '\t' + oldContent.substring(selectionEndPos)
+
+    // Set the new cursor position (current position + 1 to account for the new tab character).
+    this.selectionStart = this.selectionEnd = selectionStartPos + 1
+
+    // Prevent the default action (tabbing to the next field or control).
+    event.preventDefault()
+  }
+})
+
+/* $('.option-buttons .save').on('click', function () {
+  file.save($('.text').val(), (error) => {
+    if (error) return error
+  })
+}) */
+
+if (params && params.command === 'openFile' && params.data) {
+  openFile(params.data)
+}
